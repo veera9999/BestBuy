892ac65d2842c53eb0143d3c57eb978190b1815b
chinmay pawar <cpawar2@uic.edu>
2022-10-15 15:33:15 -0500

    added product class

---
+++ src/main/java/bestbuy/model/Product.java
@@ -0,0 +1,12 @@
+package bestbuy.model;
+
+public class Product {
+    String productID;
+    String productName;
+    String productCategory;
+    String productDescription;
+    double productPrice;
+    String manufacturer;
+
+
+}
